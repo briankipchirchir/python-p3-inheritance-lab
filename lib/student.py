d9112fbeb5193ed1927f99640a91a8f92848717c
briankipchirchir <kbrianngetich@gmail.com>
2024-09-02 11:24:19 +0300

    kk

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    def _init_(self, first_name, last_name):
+        self.first_name=first_name
+        self.last_name=last_name
+        super()._init_(first_name,last_name)
+        self.knowledge=[]
     
-    def learn(self):
-        pass
+    def learn(self,new_knowlege):
+        self.knowledge.append(new_knowlege)
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,22 @@ from user import User
 import random
 
 class Teacher(User):
+      def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        super()._init_(first_name,last_name)
+        self.knowledge=[
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
-    def teach(self):
-        pass
+      def teach(self):
+        random_index = random.randint(0, len(self.knowledge) - 1)
+        # Return the knowledge at the randomly chosen index
+        return self.knowledge[random_index]
